684574f2a8653b07ffc3eb1e703b52c48d9f3fe6
patrickfrey <patrickpfrey@yahoo.com>
2017-08-14 10:32:45 +0200

    adding source files

---
+++ cmake/build_rules.cmake
@@ -0,0 +1,41 @@
+# -----------------------------------------------------------------------------------------------
+# Defines the flags for compiler and linker and some build environment settings
+# -----------------------------------------------------------------------------------------------
+
+if(CMAKE_COMPILER_IS_GNUCXX)
+set( CMAKE_BUILD_WITH_INSTALL_RPATH FALSE )
+set( CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}/papuga" )
+set( CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE )
+endif()
+
+set_property(GLOBAL PROPERTY rule_launch_compile ccache)
+set_property(GLOBAL PROPERTY rule_launch_link ccache)
+
+if(CMAKE_COMPILER_IS_GNUCXX)
+set( PAPUGA_OPTIMIZATION_LEVEL "2" )
+set( CMAKE_CXX_FLAGS "-std=c++98  -Wall -pedantic -g -Wfatal-errors -fvisibility=hidden -fPIC -O${PAPUGA_OPTIMIZATION_LEVEL}" )
+set( CMAKE_C_FLAGS "-std=c99 -Wall -pedantic -Wfatal-errors -fPIC -O${PAPUGA_OPTIMIZATION_LEVEL}" )
+endif()
+if(CMAKE_CXX_COMPILER MATCHES ".*clang")
+set( PAPUGA_OPTIMIZATION_LEVEL "2" )
+set( CMAKE_CXX_FLAGS "-std=c++98  -Wall -pedantic -g -Wfatal-errors -fvisibility=hidden -Wno-unused-private-field -fPIC -O${PAPUGA_OPTIMIZATION_LEVEL}" )
+set( CMAKE_C_FLAGS "-std=c99 -Wall -pedantic -Wfatal-errors -fPIC -O${PAPUGA_OPTIMIZATION_LEVEL}" )
+endif()
+
+if(WIN32)
+set(CMAKE_CXX_FLAGS " ${CMAKE_CXX_FLAGS} /D_WIN32_WINNT=0x0504")
+else()
+set( CMAKE_THREAD_PREFER_PTHREAD TRUE )
+find_package( Threads REQUIRED )
+if(CMAKE_USE_PTHREADS_INIT)
+set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread" )
+set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pthread" )
+endif()
+endif()
+
+if(APPLE)
+   set(CMAKE_MACOSX_RPATH ON)
+   set( CMAKE_BUILD_WITH_INSTALL_RPATH FALSE )
+   set( CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${LIB_INSTALL_DIR}/papuga" )
+   set( CMAKE_INSTALL_RPATH_USE_LINK_PATH FALSE )
+endif(APPLE)
---
+++ include/private/internationalization.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2014 Patrick P. Frey
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#ifndef _STRUS_INTERNATIONALIZATION_H_INCLUDED
+#define _STRUS_INTERNATIONALIZATION_H_INCLUDED
+#include <libintl.h>
+
+#define _TXT(STRING) gettext(STRING)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// \brief Declare the message domain used by this package for the exception constructors declared in this module for gettext
+void papuga_initMessageTextDomain(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+
