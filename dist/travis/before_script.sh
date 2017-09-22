9171865ecdc9e824fe291148e310989094929e83
patrickfrey <patrickpfrey@yahoo.com>
2017-09-22 10:34:27 +0200

    added files for Travis build

---
+++ dist/travis/before_script.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+set -e
+
+OS=$(uname -s)
+
+case $OS in
+	Linux)
+		sudo apt-get update -qq
+		sudo apt-get install -y \
+			cmake \
+			libboost-all-dev \
+			python3-dev
+		;;
+
+	Darwin)
+		brew update
+		if test "X$CC" = "Xgcc"; then
+			brew install gcc48 --enable-all-languages || true
+			brew link --force gcc48 || true
+		fi
+		brew install \
+			cmake \
+			boost \
+			python3 \
+			|| true
+		# make sure cmake finds the brew version of gettext
+		brew link --force gettext || true
+		;;
+
+	*)
+		echo "ERROR: unknown operating system '$OS'."
+		;;
+esac
+
