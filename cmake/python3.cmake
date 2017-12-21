fbe7120aac6c4c86aa2dfa0a9be0b39f1f32d97b
patrickfrey <patrickpfrey@yahoo.com>
2017-12-21 09:30:34 +0100

    cmake: python relocation fix

--- cmake/python3.cmake
+++ cmake/python3.cmake
@@ -19,9 +19,12 @@ endforeach( PYVERSION "3.9" "3.8" "3.7" "3.6" "3.5" "3.4" "3.3" "3.2" "3.1" "3.0
 endif( NOT PYTHON_EXECUTABLE_ROOT )
 
 if( NOT PYTHON_EXECUTABLE_ROOT )
-find_package( PythonInterp 3 REQUIRED )
+set(Python_ADDITIONAL_VERSIONS 3.9 3.8 3.7 3.6 3.5 3.4 3.3 3.2 3.1)
+find_package( PythonInterp 3 )
+if ( PYTHONINTERP_FOUND )
 set( PYTHON_EXECUTABLE_ROOT ${PYTHON_EXECUTABLE} )
 MESSAGE( STATUS "Found python package executable ${PYTHON_EXECUTABLE_ROOT}" )
+endif ( PYTHONINTERP_FOUND )
 endif( NOT PYTHON_EXECUTABLE_ROOT)
 
 if (NOT PYTHON_EXECUTABLE_ROOT)
