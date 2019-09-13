a6ec681d8a95692cdb832f8c227ed2c0cac5697d
PatrickFrey <patrickpfrey@yahoo.com>
2019-09-13 10:57:01 +0200

    reverting changes and switching to another OSX image

--- dist/travis/before_script.sh
+++ dist/travis/before_script.sh
@@ -23,7 +23,7 @@ case $OS in
 	Darwin)
 		brew update
 		brew upgrade cmake
-		brew install boost@1.70
+		brew upgrade boost
 		if test "x$PAPUGA_WITH_PHP" = "xYES"; then
 			brew install openssl php71 || true
 		fi
